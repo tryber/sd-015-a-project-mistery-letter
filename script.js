1a7daed48d91ddc202855b85ab89509cbde904a0
Rafael Augusto Scherer <rafaelaugustscherer@gmail.com>
2021-08-17 18:38:35 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* Cria elementos para fazer a carta */
+
 const cartaInput = document.getElementById('carta-texto');
 const cartaP = document.getElementById('carta-gerada');
 const criarCartaButton = document.getElementById('criar-carta');
@@ -7,13 +9,16 @@ function criarCarta() {
 
   const palavrasArray = cartaInput.value.split(' ');
   console.log(palavrasArray);
+  if (palavrasArray.join('') === '') {
+    cartaP.innerText = 'Por favor, digite o conteúdo da carta.';
+  } else {
+    palavrasArray.forEach((palavra) => {
+      const spanEl = document.createElement('span');
+      spanEl.innerText = `${palavra}`;
 
-  palavrasArray.forEach((palavra) => {
-    const spanEl = document.createElement('span');
-    spanEl.innerText = `${palavra}`;
-
-    cartaP.appendChild(spanEl);
-  });
+      cartaP.appendChild(spanEl);
+    });
+  }
 }
 
 criarCartaButton.addEventListener('click', criarCarta);
