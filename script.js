3c496b35ba91b975feed65336eefb86933270c7c
Rodrigo Lima <limarodrigoo@outlook.com>
2021-08-17 16:05:01 -0300

    add error message while no text input

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
+function createLetterHelper(textArray, letter) {
+  for (let i = 0; i < textArray.length; i += 1) {
+    const newSpan = document.createElement('span');
+
+    newSpan.innerText = `${textArray[i]}`;
+    console.log(newSpan.innerHTML);
+    letter.appendChild(newSpan);
+  }
+}
+
 function createLetter() {
   const createBtn = document.getElementById('criar-carta');
 
   createBtn.addEventListener('click', () => {
     const text = document.getElementById('carta-texto').value;
-    const newSpan = document.createElement('span');
+    const textArray = text.split(' ');
+    const errorSpan = document.createElement('span');
+    const letter = document.getElementById('carta-gerada');
 
-    newSpan.innerHTML = text;
-    document.getElementById('carta-gerada').appendChild(newSpan);
+    letter.innerHTML = '';
+    errorSpan.innerHTML = 'Por favor, digite o conteúdo da carta.';
+    if (text === '' || text === ' ') {
+      letter.appendChild(errorSpan);
+    }
+    createLetterHelper(textArray, letter);
   });
 }
 
