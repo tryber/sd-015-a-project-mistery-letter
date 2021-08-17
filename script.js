b43c27538842f02008f0d0f857bd49fbc24afde5
Luan Vitor <luan_vittor@hotmail.com>
2021-08-16 23:35:35 -0300

    All done

--- script.js
+++ script.js
@@ -1,37 +1,36 @@
+const input = document.getElementById('carta-texto');
+const cartaGerada = document.getElementById('carta-gerada');
+const botao = document.getElementById('criar-carta');
+const p = document.getElementById('carta-contador');
 
-let input = document.getElementById('carta-texto');
-let cartaGerada = document.getElementById('carta-gerada');
-let botao = document.getElementById('criar-carta');
-let p = document.getElementById("carta-contador");
+const somaClasseEstilo = ['newspaper', 'magazine1', 'magazine2'];
+const somaClasseTamanho = ['medium', 'big', 'reallybig'];
+const somaClasseRotacao = ['rotateleft', 'rotateright'];
+const somaClasseSkew = ['skewleft', 'skewright'];
 
-let somaClasseEstilo = ["newspaper", "magazine1", "magazine2"];
-let somaClasseTamanho = ["medium", "big", "reallybig"];
-let somaClasseRotacao = ["rotateleft", "rotateright"];
-let somaClasseSkew = ["skewleft", "skewright"];
+const estilo = parseInt(Math.random() * 3, 10);
+const tamanho = parseInt(Math.random() * 3, 10);
+const rotacao = parseInt(Math.random() * 2, 10);
+const inclinacao = parseInt(Math.random() * 2, 10);
 
 function createLetter() {
-  let split = input.value.split(' ')
-  console.log(split)
-  cartaGerada.innerHTML = ''
-  if (input.value === "" | input.value === " ") {
-    cartaGerada.innerText = "Por favor, digite o conteúdo da carta."
+  const split = input.value.split(' ');
+  console.log(split);
+  cartaGerada.innerHTML = '';
+  if (input.value === '' || input.value === ' ') {
+    cartaGerada.innerText = 'Por favor, digite o conteúdo da carta.';
   } else {
-    for (let elemento of split) {
-      let creatSpan = document.createElement('span');
-      let estilo = parseInt(Math.random() * 3)
-      let tamanho = parseInt(Math.random() * 3)
-      let rotacao = parseInt(Math.random() * 2)
-      let inclinacao = parseInt(Math.random() * 2)
-      creatSpan.className = somaClasseEstilo[estilo]
-      creatSpan.className = somaClasseTamanho[tamanho]
-      creatSpan.className = somaClasseRotacao[rotacao]
-      creatSpan.className = somaClasseSkew[inclinacao]
+    for (let i = 0; i < split.length; i += 1) {
+      const creatSpan = document.createElement('span');
+      creatSpan.className = somaClasseEstilo[estilo];
+      creatSpan.className = somaClasseTamanho[tamanho];
+      creatSpan.className = somaClasseRotacao[rotacao];
+      creatSpan.className = somaClasseSkew[inclinacao];
       cartaGerada.appendChild(creatSpan);
-      creatSpan.innerText = elemento
+      creatSpan.innerText = split[i];
     }
   }
-  p.innerText = split.length
+  p.innerText = split.length;
 }
 
 botao.addEventListener('click', createLetter);
-
