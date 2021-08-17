31e1fbd61a4985956ad7f1469822959e27a9164d
Leandro <Playertox1c@gmail.com>
2021-08-17 15:28:31 -0300

    lint js

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
 //  Gerar estilo
-function estilo(element) {
-  let random1 = Math.floor(Math.random() * 3)
-  let random2 = Math.floor(Math.random() * 3)
-  let random3 = Math.floor(Math.random() * 2)
-  let random4 = Math.floor(Math.random() * 2)
-  console.log(random2, random1)
-  const primeiroEstilo = [`newspaper`, `magazine1`, `magazine2`]
-  const segundoEstilo = [`medium`, `big`, `reallybig`]
-  const terceiroEstilo = [`rotateleft`, `rotateright`]
-  const quartoEstilo = [`skewleft`, `skewright`]
-  return element.className = `${segundoEstilo[random1]} ${primeiroEstilo[random2]} ${terceiroEstilo[random3]} ${quartoEstilo[random4]}`
+function estilo() {
+  const random1 = Math.floor(Math.random() * 3);
+  const random2 = Math.floor(Math.random() * 3);
+  const random3 = Math.floor(Math.random() * 2);
+  const random4 = Math.floor(Math.random() * 2);
+  const A = ['newspaper', 'magazine1', 'magazine2'];
+  const B = ['medium', 'big', 'reallybig'];
+  const C = ['rotateleft', 'rotateright'];
+  const D = ['skewleft', 'skewright'];
+  return `${B[random1]} ${A[random2]} ${C[random3]} ${D[random4]}`;
 }
 
 //  Gerar spans
 function geradorDeSpan() {
   const button = document.getElementById('criar-carta');
   const pCarta = document.getElementById('carta-gerada');
-  const pContador = document.getElementById('carta-contador')
+  const pContador = document.getElementById('carta-contador');
   button.addEventListener('click', (_) => {
-    pCarta.innerHTML = ''
-    const input = document.getElementById('carta-texto').value.trim().split(' ')
+    pCarta.innerHTML = '';
+    const input = document.getElementById('carta-texto').value.trim().split(' ');
     if (input[0] !== '') {
       input.forEach((e) => {
         const span = document.createElement('span');
         span.innerHTML = e;
-        estilo(span)
+        span.className = estilo();
         pCarta.appendChild(span);
       });
-      pContador.innerHTML = pCarta.children.length
+      pContador.innerHTML = pCarta.children.length;
     } else {
-      pCarta.innerHTML = 'Por favor, digite o conteúdo da carta.'
+      pCarta.innerHTML = 'Por favor, digite o conteúdo da carta.';
     }
   });
 }
@@ -37,9 +36,9 @@ geradorDeSpan();
 
 //  Gerar novo estilo
 function mudarEstiloClick() {
-  let parent = document.getElementById('carta-gerada')
-  parent.addEventListener('click', e => {
-    e.target.className = estilo(e.target)
-  })
+  const parent = document.getElementById('carta-gerada');
+  parent.addEventListener('click', (e) => {
+    e.target.className = estilo();
+  });
 }
-mudarEstiloClick()
+mudarEstiloClick();
