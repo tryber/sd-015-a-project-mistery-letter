abdd0cd46d1f31b6a1c60326247a44db384f7c75
leonardodfelix <leonardo.doria.felix1983@gmail.com>
2021-08-16 22:48:10 -0300

    Requisito 5 ao 15 feitos

--- script.js
+++ script.js
@@ -4,12 +4,20 @@ const textoInput = document.getElementById('carta-texto');
 const pCarta = document.getElementById('carta-gerada');
 const criarBtn = document.getElementById('criar-carta');
 
+function checkInput() {
+  return (textoInput.value === '' || textoInput.value.match(/[^\s]/g) === null);
+}
+
 criarBtn.addEventListener('click', () => {
   const letterWords = textoInput.value.split(' ');
   pCarta.innerHTML = '';
-  for (let i = 0; i < letterWords.length; i += 1) {
-    const word = document.createElement('span');
-    word.innerText = letterWords[i];
-    pCarta.appendChild(word);
+  if (checkInput()) {
+    pCarta.innerText = 'Por favor, digite o conteúdo da carta.';
+  } else {
+    for (let i = 0; i < letterWords.length; i += 1) {
+      const word = document.createElement('span');
+      word.innerText = letterWords[i];
+      pCarta.appendChild(word);
+    }
   }
 });
