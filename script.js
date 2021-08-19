51578c7c25dcca70c42b3ef2c6ba5c832ff64494
lucas-morais <lucasmoraisss@gmail.com>
2021-08-19 08:40:56 -0300

    requisito 5

--- script.js
+++ script.js
@@ -2,20 +2,28 @@ const createButton = document.getElementById('criar-carta');
 const letterParagraph = document.getElementById('carta-gerada');
 const letterInput = document.getElementById('carta-texto');
 
+function createLetterParagraph(word) {
+  const span = document.createElement('span');
+  span.className = 'word';
+  span.innerHTML = word;
+  letterParagraph.appendChild(span);
+}
+
 function createLetter() {
   createButton.addEventListener('click', () => {
     if (letterParagraph.hasChildNodes()) {
       letterParagraph.innerHTML = '';
     }
 
-    const text = letterInput.value.split(' ');
+    const text = letterInput.value;
 
-    text.forEach((word) => {
-      const span = document.createElement('span');
-      span.className = 'word';
-      span.innerHTML = word;
-      letterParagraph.appendChild(span);
-    });
+    if (text.trim() === '') {
+      createLetterParagraph('Por favor, digite o conteúdo da carta.');
+    } else {
+      text.split(' ').forEach((word) => {
+        createLetterParagraph(word);
+      });
+    }
   });
 }
 
