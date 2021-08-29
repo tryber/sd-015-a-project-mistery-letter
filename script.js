f500106c50c1b1e5cb145dfb693ae0c6a0c3f793
Guarize <pedroguarize@hotmail.com>
2021-08-29 20:19:53 -0300

    Requirement - 17

--- script.js
+++ script.js
@@ -2,17 +2,31 @@ const letter = document.getElementById('carta-gerada');
 const btn = document.getElementById('criar-carta');
 const input = document.getElementById('carta-texto');
 
+const style = ['newspaper', 'magazine1', 'magazine2'];
+const size = ['medium', 'big', 'reallybig'];
+const rotate = ['rotateleft', 'rotateright'];
+const inclination = ['skewleft', 'skewright'];
+
+const randomIndex3 = () => Math.floor(Math.random() * 3);
+const randomIndex2 = () => Math.floor(Math.random() * 2);
+
+const createSpan = () => {
+  const words = input.value.split(' ');
+
+  for (let i = 0; i < words.length; i += 1) {
+    const span = document.createElement('span');
+    span.innerText = words[i];
+    span.classList = `${style[randomIndex3()]} ${size[randomIndex3()]}
+     ${rotate[randomIndex2()]} ${inclination[randomIndex2()]}`;
+    letter.appendChild(span);
+  }
+};
+
 btn.addEventListener('click', () => {
   letter.innerText = '';
   if (!input.value.trim()) {
     letter.innerText = 'Por favor, digite o conteúdo da carta.';
   } else {
-    const words = input.value.split(' ');
-
-    for (let i = 0; i < words.length; i += 1) {
-      const span = document.createElement('span');
-      span.innerText = words[i];
-      letter.appendChild(span);
-    }
+    createSpan();
   }
 });
