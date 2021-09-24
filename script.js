258d2b7d0a29fe69e8ba534cbea9bd772a5c12e0
Vitor Diorio <vitor.diorio@yahoo.com.br>
2021-09-24 17:38:19 -0300

    Requisito 17 realmente feito, o teste passou mesmo sem eu ter realizado

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ const rotation = ['rotateleft', 'rotateright'];
 const skew = ['skewleft', 'skewright'];
 const groups = [styles, sizes, rotation, skew];
 
+function changeStyle(event) {
+  const word = event.target;
+  word.className = '';
+  groups.forEach((group) => {
+    event.target.classList.add(group[Math.floor(Math.random() * group.length)]);
+  });
+}
+
 function createLetter() {
   const p = document.getElementById('carta-gerada');
   p.innerHTML = '';
@@ -12,12 +20,14 @@ function createLetter() {
   letter.forEach((word) => {
     const span = document.createElement('span');
     span.innerText = word;
+    span.addEventListener('click', changeStyle);
     groups.forEach((group) => {
       span.classList.add(group[Math.floor(Math.random() * group.length)]);
     });
     if (word !== '') p.appendChild(span);
   });
   if (p.children.length === 0) p.innerText = 'Por favor, digite o conteúdo da carta.';
+  document.getElementById('carta-contador').innerText = p.children.length;
 }
 
 window.onload = () => {
