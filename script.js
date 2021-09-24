e4156ac742de709624e0e7d00317d0a9a6d6280e
Vitor Diorio <vitor.diorio@yahoo.com.br>
2021-09-24 17:25:08 -0300

    Requisitos 16 e 17

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+const styles = ['newspaper', 'magazine1', 'magazine2'];
+const sizes = ['medium', 'big', 'reallybig'];
+const rotation = ['rotateleft', 'rotateright'];
+const skew = ['skewleft', 'skewright'];
+const groups = [styles, sizes, rotation, skew];
+
 function createLetter() {
   const p = document.getElementById('carta-gerada');
   p.innerHTML = '';
   const words = document.getElementById('carta-texto').value;
   const letter = words.split(' ');
   letter.forEach((word) => {
-    const letterWord = document.createElement('span');
-    letterWord.innerText = word;
-    if (word !== '') p.appendChild(letterWord);
+    const span = document.createElement('span');
+    span.innerText = word;
+    groups.forEach((group) => {
+      span.classList.add(group[Math.floor(Math.random() * group.length)]);
+    });
+    if (word !== '') p.appendChild(span);
   });
   if (p.children.length === 0) p.innerText = 'Por favor, digite o conteúdo da carta.';
 }
