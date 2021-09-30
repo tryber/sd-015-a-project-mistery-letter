24ba3fff6dc9de64ea3cb1968cc24394f59028d2
alexandrexpj <alexandrexpjr@hotmail.com>
2021-09-30 20:08:55 -0300

    req 17

--- script.js
+++ script.js
@@ -4,14 +4,34 @@ const getInput = document.getElementById('carta-texto');
 
 getGenerateLetterButton.addEventListener('click', generateLetter)
 
-function generateLetter() {
-  getGeneratedLetter.innerHTML = "Por favor, digite o conteúdo da carta.";
+const classes = {
+  estilo: [`newspaper`, `magazine1`, `magazine2`],
+  tamanho: [`medium`, `big`, `reallybig`],
+  rotacao: [`rotateleft`, `rotateright`],
+  inclinacao: [`skewleft`, `skewright`],
+}
 
-  const letterWords = getInput.value.split(' ');
-  
+function randomNumber(max) {
+  return Math.floor(Math.random() * (max));
+}
+
+const { estilo, tamanho, rotacao, inclinacao } = classes;
+
+const arrayOfClasses = [...estilo, ...tamanho, ...rotacao, ...inclinacao];
+getGeneratedLetter.innerText = "Por favor, digite o conteúdo da carta.";
+
+function generateLetter() {
+  getGeneratedLetter.innerHTML = '';
+  const input = getInput.value.trim();
+  if (input.length === 0) return getGeneratedLetter.innerHTML = "Por favor, digite o conteúdo da carta.";
+
+  const letterWords = input.split(' ');
+
   letterWords.forEach(word => {
     const span = document.createElement('span');
-    span.innerText = word;
+    span.innerHTML = word;
+    span.className = `${estilo[randomNumber(3)]} ${tamanho[randomNumber(3)]} ${rotacao[randomNumber(2)]} ${inclinacao[randomNumber(2)]}`;
     getGeneratedLetter.appendChild(span);
   })
 }
+
